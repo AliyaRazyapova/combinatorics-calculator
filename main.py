ec80fcd7a9ff7c93f41c8029d062075753102ea0
Razyapova Aliya <a.raziapova@mail.ru>
2022-11-22 10:58:04 +0300

    add link to main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import datetime
 from flask import Flask, make_response, session, request, render_template, redirect, url_for
 
 
 app = Flask(__name__)
 app.secret_key = "111"
+
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+
+
+@app.route("/")
+def main_page():
+    return render_template('main.html.')
 
 
 if __name__ == '__main__':
